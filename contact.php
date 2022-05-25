b9a0ea42017db3ab1077c0d201f3f00cf74d600c
MilesAngels <mangels@alphaworks.tech>
2022-05-25 14:01:33 -0700

    contact

---
+++ contact.php
@@ -0,0 +1,31 @@
+<?php 
+    /** 
+     * 
+     * Template Name: Contact
+     * 
+     * @package TravelSite
+     * 
+     **/
+?>
+
+<?php get_header(); ?>
+
+<div class="form">
+    <form action="#" class="contact-form">
+        <h2>
+            Please Contact Us
+        </h2>
+        <label for="fName">First Name</label>
+        <input type="text">
+        <label for="lName">Last Name</label>
+        <input type="text">
+
+        <label for="eMail">Email</label>
+        <input type="email" name="" id="">
+
+        <label for="message">Message</label>
+        <textarea name="" id=""></textarea>
+    </form>
+</div>
+
+<?php get_footer();?>
