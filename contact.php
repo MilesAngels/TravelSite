35804799fd328ac98297cd2b9bcbd6ff3062e243
MilesAngels <mangels@alphaworks.tech>
2022-05-26 13:26:41 -0700

    adding stylings to contact form

--- contact.php
+++ contact.php
@@ -10,26 +10,38 @@
 
 <?php get_header(); ?>
 <?php get_template_part("template-parts/hero"); ?>
-<div class="form">
-    <form action="#" class="contact-form">
-        <h2>
-            Please Contact Us
-        </h2>
-        <label for="fName">First Name</label>
-        <input type="text">
-        <label for="lName">Last Name</label>
-        <input type="text">
 
-        <label for="eMail">Email</label>
-        <input type="email" name="" id="">
+<!-- Start of Contact Form -->
+<div class="contact-section row">
+    <form action="#" class="contact-form contact-column">
+        <div class="form-content">
+            <h2>
+                Please Contact Us
+            </h2>
+            <label for="fName">First Name</label>
+            <input type="text">
+            <label for="lName">Last Name</label>
+            <input type="text">
 
-        <label for="message">Message</label>
-        <textarea name="message" id="message"></textarea>
+            <label for="eMail">Email</label>
+            <input type="email" name="" id="">
 
-        <input type="submit">
+            <label for="message">Message</label>
+            <textarea name="message" id="message"></textarea>
+
+            <input type="submit">
+        </div>
     </form>
-</div>
 
+    <aside class="contact-column">
+        <ul>
+            <li>Email</li>
+            <li>Phone</li>
+            <li>Adress</li>
+        </ul>
+    </aside>
+</div>
+<!-- End of Contact Form -->
 <?php get_template_part("template-parts/banner");?>
 <?php get_template_part("template-parts/to-top");?>
 <?php get_footer();?>
