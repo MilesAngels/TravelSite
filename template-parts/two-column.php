51a2cc7fb933d2f703f3aa3a44f63aa9b1509438
MilesAngels <mangels@alphaworks.tech>
2022-05-31 09:57:10 -0700

    adding styles to about-page

--- template-parts/two-column.php
+++ template-parts/two-column.php
@@ -1,8 +1,21 @@
-<div class="row">
-    <aside>
-        Hello
-    </aside>
-    <article>
-        Job
-    </article>
+<div class="two-row">
+    <div class="two-column">
+        <?php get_template_part('sidebar');?>
+    </div>
+    <div class="two-column">
+        <div>
+           <h2><em>About Us</em></h2>
+           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
+        </div>
+        <div>
+            <h3>Our Story</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="row">
+            <img class="column portait-img" src="https://travel-site.local/wp-content/uploads/2022/05/pexels-trace-hudson-2724664.jpg" alt="snowy mountain">
+            <img class="column portrait-img" src="https://travel-site.local/wp-content/uploads/2022/05/pexels-oliver-sjostrom-931007-scaled.jpg" alt="waterfall">
+            <img class="column portrait-img" src="https://travel-site.local/wp-content/uploads/2022/05/pexels-vishal-amin-910307.jpg" alt="lake at base of mountain">
+        </div>
+    </div>
 </div>
