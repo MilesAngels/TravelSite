227ff08831ea37becb603901936d4045f00467c8
MilesAngels <mangels@alphaworks.tech>
2022-06-01 12:00:02 -0700

    adding style to blog page template

--- template-parts/blog-page.php
+++ template-parts/blog-page.php
@@ -1,7 +1,13 @@
+<section class="container">
 <?php /* Start the Loop */
 	while ( have_posts() ) : ?>
 	<?php the_post(); ?>
-        <h2><?php echo get_the_title(); ?></h2>
-        <p><?php the_excerpt(); ?></p>
-        <button class="moreBtn"><a href="">Read more..</a></button>
+        <div class="card">
+            <h2><?php echo get_the_title(); ?></h2>
+            <img src="<?php the_post_thumbnail();?>
+            <p><?php the_excerpt(); ?></p>
+            <button class="moreBtn"><a href="<?php the_permalink();?>">Read more..</a></button>
+        </div>
+        
 	<?php endwhile; ?>
+</section>
