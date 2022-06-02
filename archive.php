79e69a1db63ce8ea13cc6175928609f70f320d8b
MilesAngels <mangels@alphaworks.tech>
2022-06-02 08:14:38 -0700

    adding archive template

--- archive.php
+++ archive.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying archive pages
+ * Template Name: Archives
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -9,43 +9,34 @@
 
 get_header();
 ?>
+<main id="primary" class="site-main">
 
-	<main id="primary" class="site-main">
+<?php if (have_posts()) : ?>
+	<?php while ( have_posts() ) : ?>
+		<?php  the_post(); ?>
 
-		<?php if ( have_posts() ) : ?>
+			<h2>Sample</h2>
+		<?php //get_template_part( 'template-parts/content', get_post_type() );
 
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="archive-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
+		the_post_navigation(
+			array(
+				'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'travel-site' ) . '</span> <span class="nav-title">%title</span>',
+				'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'travel-site' ) . '</span> <span class="nav-title">%title</span>',
+			)
+		);
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
+		// If comments are open or we have at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) :
+			comments_template();
 		endif;
-		?>
 
-	</main><!-- #main -->
+	endwhile; // End of the loop.
+endif;
+	?>
+
+</main><!-- #main -->
 
 <?php
 get_sidebar();
 get_footer();
+
---
+++ template-parts/archive-template.php
@@ -0,0 +1,5 @@
+<?php while(have_posts()):?>
+    <?php the_post();?>
+    Hello
+<?php endwhile;?>
+<?php get_sidebar(); ?>
